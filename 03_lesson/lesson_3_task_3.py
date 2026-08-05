d46b04e2c1cac48872763248cf9d8e10285b62be
Надежда <Nadezda@exsample.com>
2026-08-06 00:10:53 +0300

    Исправлен вывод в консоле

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -7,7 +7,4 @@ from_address = Address("654321", "Санкт-Петербург", "Невски�
 
 mailing = Mailing(to_address, from_address, 350.50, "TRACK123456")
 
-print("Отправление" + mailing.track + " из " +
-      mailing.from_address.index + ", " + mailing.from_address.city + ", " +
-      mailing.from_address.street + ", " + mailing.from_address.house + " - " +
-      mailing.from_address.apartment)
+print(f"Отправление {mailing.track} из {mailing.from_address.index}, {mailing.from_address.city}, {mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartment} в {mailing.to_address.city}, {mailing.to_address.street}, {mailing.to_address.house} - {mailing.to_address.apartment}. Стоимость {mailing.cost} рублей.")
